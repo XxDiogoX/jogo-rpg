ac5ed8480dc491f1753f2ff91c17c68372c68a9d
XxDiogoX <diogorodriguesk02@gmail.com>
2022-06-01 18:46:29 -0300

    Implementado o metodo atacarInimigo na Classe Jogador

--- personagens/Jogador.js
+++ personagens/Jogador.js
@@ -10,4 +10,15 @@ export class Jogador extends Personagem{
         this._expParaProximoNivel = 200;
 
     }
+
+    
+
+    atacarInimigo(inimigo){
+        if(inimigo instanceof Inimigo){
+            this.atacar(inimigo);
+        }else{
+            console.log("Você não pode atacar, porquê não um inimigo!");
+        }
+
+    }
 }
