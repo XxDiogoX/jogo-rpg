b7259f5b6dd69e24ea63fffebe14cd2cf0cb772c
XxDiogoX <diogorodriguesk02@gmail.com>
2022-06-03 18:05:55 -0300

    Declarada a classe Armadura e seu metodo construtor

---
+++ personagens/itens/Armadura.js
@@ -0,0 +1,8 @@
+export class Armadura{
+    constructor(nome,defesa,descricao,defesaElemental){
+        this._nome = nome;
+        this._defesa = defesa;
+        this._descricao = descricao;
+        this._defesaElemental = defesaElemental;
+    }
+}
