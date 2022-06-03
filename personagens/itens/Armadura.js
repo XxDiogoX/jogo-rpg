14a8e93733d7e8b860b5da80fb1e7bcd0e25ec37
XxDiogoX <diogorodriguesk02@gmail.com>
2022-06-03 18:23:16 -0300

    Declarado o metodo get da classe Armadura

--- personagens/itens/Armadura.js
+++ personagens/itens/Armadura.js
@@ -1,8 +1,37 @@
 export class Armadura{
+    //Classe abstrata somente para herança
     constructor(nome,defesa,descricao,defesaElemental){
-        this._nome = nome;
-        this._defesa = defesa;
-        this._descricao = descricao;
-        this._defesaElemental = defesaElemental;
+        if(this.constructor === Armadura){
+            throw new Error ("Essa classe não pode ser instanciada diretamente!");
+        
+        }else if(this.constructor === ArmaduraPeitoral){
+            this._nome = nome;
+            this._defesa = defesa;
+            this._descricao = descricao;
+            this._defesaElemental = defesaElemental;
+        }else if(this.constructor === Escudo){
+            this._nome = nome;
+            this._defesa = defesa;
+            this._descricao = descricao;
+            this._defesaElemental = defesaElemental;
+
+        }
+        
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    get defesa(){
+        return this._defesa;
+    }
+
+    get descricao(){
+        return this._descricao;
+    }
+
+    get defesaElemental(){
+        return this._defesaElemental;
     }
 }
